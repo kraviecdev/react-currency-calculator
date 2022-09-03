d603b2a9389051647fd2ebd1eec714302ab05233
Patryk Krawczyk <patrick36212@gmail.com>
2022-09-03 17:58:09 +0200

    del currencies reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from './common/Header/ThemeSwitch/themeSlice';
-import currenciesReducer from "./features/calculatorForm/currenciesSlice";
 
 export default configureStore({
     reducer: {
         theme: themeReducer,
-        currencies: currenciesReducer,
     },
 });
