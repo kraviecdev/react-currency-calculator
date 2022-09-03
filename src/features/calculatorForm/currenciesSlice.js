ec34618592facafc0a9e3f5a1988d9021f7a656e
Patryk Krawczyk <patrick36212@gmail.com>
2022-09-03 17:05:59 +0200

    change initial state

--- src/features/calculatorForm/currenciesSlice.js
+++ src/features/calculatorForm/currenciesSlice.js
@@ -3,23 +3,23 @@ import { createSlice } from "@reduxjs/toolkit";
 const currenciesSlice = createSlice({
     name: "currencies",
     initialState: {
+        amount: '',
         ownedCurrency: "EUR",
         targetCurrency: "USD",
         result: {
-            initialAmount: '',
-            caltulatedResult: 0,
+            value: 0,
             targetRate: null,
         },
     },
     reducers: {
         handleAmountChange: (state, action) => {
-            state.result.initialAmount = action.payload;
+            state.amount = action.payload;
         },
-        handleOwnedCurrency: (state, action) => {
-            state.ownedCurrency = action.payload;
+        handleOwnedCurrency: ({ ownedCurrency }, action) => {
+            ownedCurrency = action.payload;
         },
-        handleTargetCurrency: (state, action) => {
-            state.targetCurrency = action.payload;
+        handleTargetCurrency: ({ targetCurrency }, action) => {
+            targetCurrency = action.payload;
         },
         handleSwichCurrencies: state => {
             const initialOwnedCurrency = state.ownedCurrency
@@ -38,14 +38,15 @@ export const {
     handleTargetCurrency,
     handleSwichCurrencies,
     handleResult,
+    handleTargetRate,
 } = currenciesSlice.actions
 
 const selectCurrenciesState = state => state.currencies;
-const selectResultState = state => selectCurrenciesState(state).result;
 
-export const selectAmount = state => selectResultState(state).initialAmount;
+export const selectAmount = state => selectCurrenciesState(state).amount;
 export const selectInitialResultState = state => selectCurrenciesState(state).result;
 export const selectOwnedCurrency = state => selectCurrenciesState(state).ownedCurrency;
 export const selectTargetCurrency = state => selectCurrenciesState(state).targetCurrency;
+export const selectTargetRate = state => selectCurrenciesState(state).targetRate;
 
 export default currenciesSlice.reducer;
