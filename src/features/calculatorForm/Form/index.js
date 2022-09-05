7f6b7217fbb9edc3ba1c31c3fee5ac740dc88591
Patryk Krawczyk <patrick36212@gmail.com>
2022-09-05 19:45:28 +0200

    add status components, change result to state

--- src/features/calculatorForm/Form/index.js
+++ src/features/calculatorForm/Form/index.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { Select, Input } from "../Field";
 import Result from "../Result";
+import Info from "../../../common/Info";
+import Loading from "../../../common/Info/Loading";
+import Failed from "../../../common/Info/Failed";
 import {
     StyledAnnotatnion,
     StyledWrapper,
@@ -21,11 +24,13 @@ const Form = () => {
     } = useRatesData();
 
     const [amount, setAmount] = useState();
-    const result = +amount * ratesData.targetRate;
+    const [result, setResult] = useState(0);
+    const [isSubmitClicked, setIsSubmitClicked] = useState(false);
 
     useEffect(() => {
         document.title = `Calculate from ${ownedCurrency} to ${targetCurrency}`;
-    }, [ownedCurrency, targetCurrency]);
+        setResult(+amount * ratesData.targetRate);
+    }, [ownedCurrency, targetCurrency, amount, ratesData.targetRate]);
 
     const currencySwitch = (event) => {
         event.preventDefault();
@@ -34,7 +39,7 @@ const Form = () => {
     };
 
     const calculateResult = () => {
-        return result;
+        setIsSubmitClicked(true);
     };
 
     const onFormSubmit = (event) => {
@@ -43,71 +48,80 @@ const Form = () => {
     };
 
     return (
+
         <StyledForm onSubmit={onFormSubmit}>
             <StyledFieldset>
-                <StyledAnnotatnion>*Fields required</StyledAnnotatnion>
-                <StyledWrapper>
-                    <Select
-                        fieldName={"From*: "}
-                        name={"currency"}
-                        value={ownedCurrency}
-                        onChange={({ target }) => setOwnedCurrency(target.value)}
-                        option={!!ratesData.rates && Object.keys(ratesData.rates).map((currency) => (
-                            <option
-                                key={currency}
-                                value={currency}
-                            >
-                                {currency}
-                            </option>
-                        ))}
-                    />
-                    <StyledWrapper changeButtonWrapper>
-                        <StyledButton
-                            onClick={currencySwitch}
-                            changeButton
-                        >
-                            <ChangeIcon />
-                        </StyledButton>
-                    </StyledWrapper>
-                    <Select
-                        fieldName={"To*: "}
-                        name={"currency"}
-                        value={targetCurrency}
-                        onChange={({ target }) => setTargetCurrency(target.value)}
-                        option={!!ratesData.rates && Object.keys(ratesData.rates).map((currency) => (
-                            <option
-                                key={currency}
-                                value={currency}
-                            >
-                                {currency}
-                            </option>
-                        ))}
-                    />
-                    <Input
-                        fieldName={`${ownedCurrency} Amount*: `}
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                    />
-                </StyledWrapper>
-                <StyledWrapper button>
-                    <StyledButton>Calculate</StyledButton>
-                </StyledWrapper>
-                <Result
-                    amount={+amount}
-                    ownedCurrency={ownedCurrency}
-                    ownedRate={ratesData.ownedRate}
-                    targetCurrency={targetCurrency}
-                    targetRate={ratesData.targetRate}
-                    result={result.toFixed(2)}
-                />
-                <StyledAnnotatnion date >
-                    Exchange rates valid as of:<br />
-                    <strong>
-                        {ratesData.date}
-                    </strong>
-                </StyledAnnotatnion>
+                {ratesData.state === "loading" && (<Info textInfo={"Loadnig current exchange rates from European Central Bank."} loading={<Loading />} />)}
+                {ratesData.state === "error" && (<Info textInfo={`Sorry, something went wrong. Check you Internet connection. If it's ok, the error in on our side.`} additionalInfo={`You may refresh the page or try again later.`} loading={<Failed />} />)}
+                {ratesData.state === "success" && (
+                    <>
+                        <StyledAnnotatnion>*Fields required</StyledAnnotatnion>
+                        <StyledWrapper>
+                            <Select
+                                fieldName={"From*: "}
+                                name={"currency"}
+                                value={ownedCurrency}
+                                onChange={({ target }) => setOwnedCurrency(target.value)}
+                                option={!!ratesData.rates && Object.keys(ratesData.rates).map((currency) => (
+                                    <option
+                                        key={currency}
+                                        value={currency}
+                                    >
+                                        {currency}
+                                    </option>
+                                ))}
+                            />
+                            <StyledWrapper changeButtonWrapper>
+                                <StyledButton
+                                    onClick={currencySwitch}
+                                    changeButton
+                                >
+                                    <ChangeIcon />
+                                </StyledButton>
+                            </StyledWrapper>
+                            <Select
+                                fieldName={"To*: "}
+                                name={"currency"}
+                                value={targetCurrency}
+                                onChange={({ target }) => setTargetCurrency(target.value)}
+                                option={!!ratesData.rates && Object.keys(ratesData.rates).map((currency) => (
+                                    <option
+                                        key={currency}
+                                        value={currency}
+                                    >
+                                        {currency}
+                                    </option>
+                                ))}
+                            />
+                            <Input
+                                fieldName={`${ownedCurrency} Amount*: `}
+                                value={amount}
+                                onChange={({ target }) => setAmount(target.value)}
+                            />
+                        </StyledWrapper>
+                        <StyledWrapper button>
+                            <StyledButton>Calculate</StyledButton>
+                        </StyledWrapper>
+                        <Result
+                            amount={+amount}
+                            ownedCurrency={ownedCurrency}
+                            ownedRate={ratesData.ownedRate}
+                            targetCurrency={targetCurrency}
+                            targetRate={ratesData.targetRate}
+                            result={result.toFixed(2)}
+                            active={isSubmitClicked}
+                        />
+                        <StyledAnnotatnion date >
+                            Exchange rates valid as of:<br />
+                            <strong>
+                                {ratesData.date}
+                            </strong>
+                        </StyledAnnotatnion>
+                    </>
+                )}
             </StyledFieldset>
         </StyledForm>
+
     );
 };
 
