2e19001314480c423136f72f1429bef511629e18
Patryk Krawczyk <patrick36212@gmail.com>
2022-09-10 00:56:05 +0200

    delete transition

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -7,8 +7,7 @@ export const GlobalStyle = createGlobalStyle`
             font-family: 'Lato', sans-serif;
             -webkit-font-smoothing: antialiased;
             -moz-osx-font-smoothing: grayscale;
-            box-sizing: border-box; 
-            transition: 1s cubic-bezier(0.69, 0.15, 0.31, 0.85);
+            box-sizing: border-box;
             font-size: 20px;
 
             @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
