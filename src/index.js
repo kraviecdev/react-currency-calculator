893567895f18dea34aebd4d1a5ea562f7550c6d5
patrick36212 <patrick36212@gmail.com>
2024-02-06 11:55:05 +0100

    update react to latest implementation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('calc')).render(
   <React.StrictMode>
     <Provider store={store} >
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('calc')
 );
